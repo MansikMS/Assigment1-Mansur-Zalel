d758bfdfe67d16a1e22d051e0112eed5bffbe2ad
MansikMS <mansurzalelov09@mail.ru>
2024-12-28 22:26:04 +0500

    Assigment 1
    Assigment 1

---
+++ Aassigment Mansur Zalel/src/Teacher.java
@@ -0,0 +1,38 @@
+public class Teacher extends Person {
+    private String subject;
+    private int yearsOfExperience;
+    private int salary;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void giveRaise(int percentage) {
+
+        this.salary += salary * percentage / 100.0;
+    }
+
+    public String toString() {
+        return super.toString() + ". I teach " + subject;
+    }
+}
